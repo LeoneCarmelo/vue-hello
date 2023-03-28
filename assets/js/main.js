c61e1a517cb27cb28b8f9b06a49623add06fd7a5
LeoneCarmelo <carmeloleone@outlook.it>
2023-03-28 16:03:46 +0200

    Initial commit + code

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Welcome Tutors!',
+            image: 'https://www.positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png'
+        }
+    }
+}).mount('#app')
